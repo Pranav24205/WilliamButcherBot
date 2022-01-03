d7fdc43d295d75e660f2ee843004fd6c3db85ad0
Pranav24205 <96657174+Pranav24205@users.noreply.github.com>
2022-01-03 20:59:58 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -26,21 +26,21 @@ if HEROKU:
     RSS_DELAY = int(environ.get("RSS_DELAY", None))
     PM_PERMIT = bool(int(environ.get("PM_PERMIT", None)))
 else:
-    BOT_TOKEN = "467677575:YZfaakjwd545dfg-N6JStihhuw5gQeZHntc"
-    API_ID = 123456
-    API_HASH = "dfxcgs5s12hdcxfgdfz"
+    BOT_TOKEN = "5014285587:AAHv95jed8jrvwDgLOEvQUSfAdgod7EKjCE"
+    API_ID = 18622297
+    API_HASH = "27e6993af0786f66f96599db6cd10bcc"
     USERBOT_PREFIX = "."
-    PHONE_NUMBER = "+916969696969"  # Need for Userbot
+    PHONE_NUMBER = "+13085714955"  # Need for Userbot
     SUDO_USERS_ID = [
-        4543744343,
-        543214651351,
+        1511373882,
+        163494588,
     ]  # Sudo users have full access to everything, don't trust anyone
-    LOG_GROUP_ID = -100125431255
-    GBAN_LOG_GROUP_ID = -100125431255
-    MESSAGE_DUMP_CHAT = -1001181696437
+    LOG_GROUP_ID = -1001726870449
+    GBAN_LOG_GROUP_ID = -1001726870449
+    MESSAGE_DUMP_CHAT = -1001726870449
     WELCOME_DELAY_KICK_SEC = 300
     MONGO_URL = "mongodb+srv://username:password@cluster0.ksiis.mongodb.net/YourDataBaseName?retryWrites=true&w=majority"
-    ARQ_API_KEY = "Get this from @ARQRobot"
+    ARQ_API_KEY = "SPGHHO-JIWZUR-KEQPQU-ZJFQFT-ARQ"
     ARQ_API_URL = "https://thearq.tech"
     LOG_MENTIONS = True
     RSS_DELAY = 300  # In seconds
