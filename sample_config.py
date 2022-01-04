bc2742b0f0eba1671397b75d7cbe513398349720
Pranav24205 <96657174+Pranav24205@users.noreply.github.com>
2022-01-04 20:46:42 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -9,20 +9,20 @@ HEROKU = (
 
 if HEROKU:
 
-    BOT_TOKEN = environ.get("BOT_TOKEN", None)
-    API_ID = int(environ.get("API_ID", 6))
-    API_HASH = environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-    SESSION_STRING = environ.get("SESSION_STRING", None)
+    BOT_TOKEN = environ.get("BOT_TOKEN", 5014285587:AAHv95jed8jrvwDgLOEvQUSfAdgod7EKjCE)
+    API_ID = int(environ.get("API_ID", 18622297))
+    API_HASH = environ.get("API_HASH", "27e6993af0786f66f96599db6cd10bcc")
+    SESSION_STRING = environ.get("SESSION_STRING", 1AZWarzoBu1HBcg1iJSTctkrkIJCFvC7K5jVDL6oMDM-3CbP6mclKkfNkfdwGf0a-tJpe_aI3-7_LKu786KkXHFNK-XJeDxStCTLPdKesz8RzpG4KOudgOTJcBP5tVZPeMUO-v_4cBR3zdGpyUhr7bLWiekzi5intfMEKj37LNZaMdMk6IheUcNCXIU9_GkCY29Ht23JRUq2Gcl1QziYoF8fMqnzxv1ybuX5SRvDupcDPbtltczrBQPT9yYDmrDAPZrSxVLXy4jMjf1gaDF6_SWMLoUiDMMYpQrwhtA1YBCp5Tm1DxAJDsrbLNsAVw-svL-Aqaoouzgq1SXZXi7xYmrRKOOZzGv8=)
     USERBOT_PREFIX = environ.get("USERBOT_PREFIX", ".")
     SUDO_USERS_ID = [int(x) for x in environ.get("SUDO_USERS_ID", "").split()]
     LOG_GROUP_ID = int(environ.get("LOG_GROUP_ID", -1001726870449))
-    GBAN_LOG_GROUP_ID = int(environ.get("GBAN_LOG_GROUP_ID", None))
-    MESSAGE_DUMP_CHAT = int(environ.get("MESSAGE_DUMP_CHAT", None))
+    GBAN_LOG_GROUP_ID = int(environ.get("GBAN_LOG_GROUP_ID", -1001726870449))
+    MESSAGE_DUMP_CHAT = int(environ.get("MESSAGE_DUMP_CHAT", -1001726870449))
     WELCOME_DELAY_KICK_SEC = int(environ.get("WELCOME_DELAY_KICK_SEC", None))
-    MONGO_URL = environ.get("MONGO_URL", None)
+    MONGO_URL = environ.get("MONGO_URL", mongodb://mongo:LOEgdfz9JY11BRbH9E89@containers-us-west-24.railway.app:5949)
     ARQ_API_URL = environ.get("ARQ_API_URL", None)
-    ARQ_API_KEY = environ.get("ARQ_API_KEY", None)
-    LOG_MENTIONS = bool(int(environ.get("LOG_MENTIONS", None)))
+    ARQ_API_KEY = environ.get("ARQ_API_KEY", SPGHHO-JIWZUR-KEQPQU-ZJFQFT-ARQ)
+    LOG_MENTIONS = bool(int(environ.get("LOG_MENTIONS", -1001726870449)))
     RSS_DELAY = int(environ.get("RSS_DELAY", None))
     PM_PERMIT = bool(int(environ.get("PM_PERMIT", None)))
 else:
